7f9d8b4d2e21509bf565ccbccbdf773fa9844cd0
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-04-28 11:26:59 +0600

    Moving events to editor command

---
+++ src/editor/editorcommand.rs
@@ -0,0 +1,54 @@
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+use std::{convert::TryFrom, fmt::format};
+
+use super::terminal::Size;
+
+pub enum Direction {
+    PageUp,
+    PageDown,
+    Home,
+    End,
+    Up,
+    Left,
+    Right,
+    Down,
+}
+
+pub enum EditorCommand {
+    Move(Direction),
+    Resize(Size),
+    Quit,
+}
+
+impl TryFrom<Event> for EditorCommand {
+    type Error = String;
+    fn try_from(event: Event) -> Result<Self, Self::Error> {
+        match event {
+            Event::Key(KeyEvent {
+                code, modifiers, ..
+            }) => match (code, modifiers) {
+                (KeyCode::Char('q'), KeyModifiers::CONTROL) => Ok(Self::Quit),
+                (KeyCode::Char('k'), _) => Ok(Self::Move(Direction::Up)),
+                (KeyCode::Char('j'), _) => Ok(Self::Move(Direction::Down)),
+                (KeyCode::Char('h'), _) => Ok(Self::Move(Direction::Left)),
+                (KeyCode::Char('l'), _) => Ok(Self::Move(Direction::Right)),
+                (KeyCode::PageDown, _) => Ok(Self::Move(Direction::PageDown)),
+                (KeyCode::PageUp, _) => Ok(Self::Move(Direction::PageUp)),
+                (KeyCode::Home, _) => Ok(Self::Move(Direction::Home)),
+                (KeyCode::End, _) => Ok(Self::Move(Direction::End)),
+
+                _ => Err(format!("Key Code not supported: {code:?}")),
+            },
+            Event::Resize(width_u16, height_u16) => {
+                #[allow(clippy::as_conversions)]
+                let height = height_u16 as usize;
+
+                #[allow(clippy::as_conversions)]
+                let width = width_u16 as usize;
+                Ok(Self::Resize(Size { height, width }))
+            }
+
+            _ => Err(format!("Event Not supported: {event:?}")),
+        }
+    }
+}
