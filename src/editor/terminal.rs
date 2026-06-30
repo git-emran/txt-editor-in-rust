d81c426461367db2be03852eaeb9faaf49a28fe6
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-06-30 20:41:12 +0600

    Moving text location to row and col

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -17,6 +17,16 @@ pub struct Position {
     pub col: usize,
     pub row: usize,
 }
+
+impl Position {
+    pub const fn saturating_sub(self, other: Self) -> Self {
+        Self {
+            row: self.row.saturating_sub(other.row),
+            col: self.col.saturating_sub(other.col),
+        }
+    }
+}
+
 // This is the terminal
 pub struct Terminal;
 
--- src/editor/view.rs
+++ src/editor/view.rs
@@ -1,5 +1,5 @@
 use self::line::Line;
-use std::cmp::min;
+use std::{cmp::min, panic::Location};
 
 use super::{
     editorcommand::{Direction, EditorCommand},
@@ -7,28 +7,53 @@ use super::{
 };
 mod buffer;
 use buffer::Buffer;
-mod location;
-use location::Location;
 mod line;
 
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Copy, Clone, Default)]
+pub struct Location {
+    pub grapheme_index: usize,
+    pub line_index: usize,
+}
+
 pub struct View {
     buffer: Buffer,
     needs_redraw: bool,
     size: Size,
-    location: Location,
-    scroll_offset: Location,
+    text_location: Location,
+    scroll_offset: Position,
 }
 
 impl View {
+    pub fn handle_command(&mut self, command: EditorCommand) {
+        match command {
+            EditorCommand::Resize(size) => self.resize(size),
+            EditorCommand::Move(direction) => self.move_text_location(&direction),
+            EditorCommand::Quit => {}
+        }
+    }
+
+    pub fn load(&mut self, file_name: &str) {
+        if let Ok(buffer) = Buffer::load(file_name) {
+            self.buffer = buffer;
+            self.needs_redraw = true;
+        }
+    }
+
+    fn resize(&mut self, to: Size) {
+        self.size = to;
+        self.scroll_text_location_into_view();
+        self.needs_redraw = true;
+    }
+
     pub fn render(&mut self) {
         if !self.needs_redraw {
             return;
         }
         let Size { height, width } = self.size;
-        let top = self.scroll_offset.y;
+        let top = self.scroll_offset.row;
 
         if height == 0 || width == 0 {
             return;
@@ -39,9 +64,9 @@ impl View {
 
         for current_row in 0..height {
             if let Some(line) = self.buffer.lines.get(current_row.saturating_add(top)) {
-                let left = self.scroll_offset.x;
-                let right = self.scroll_offset.x.saturating_add(width);
-                Self::render_line(current_row, &line.get(left..right));
+                let left = self.scroll_offset.col;
+                let right = self.scroll_offset.col.saturating_add(width);
+                Self::render_line(current_row, &line.get_visible_graphemes(left..right));
             } else if current_row == vertical_center && self.buffer.is_empty() {
                 Self::render_line(current_row, &Self::build_welcome_message(width));
             } else {
@@ -67,99 +92,52 @@ impl View {
         full_message
     }
 
-    pub fn handle_command(&mut self, command: EditorCommand) {
-        match command {
-            EditorCommand::Resize(size) => self.resize(size),
-            EditorCommand::Move(direction) => self.move_text_location(&direction),
-            EditorCommand::Quit => {}
-        }
-    }
-
-    pub fn load(&mut self, file_name: &str) {
-        if let Ok(buffer) = Buffer::load(file_name) {
-            self.buffer = buffer;
-            self.needs_redraw = true;
-        }
-    }
-
-    pub fn get_position(&self) -> Position {
-        self.location.subtract(&self.scroll_offset).into()
-    }
-
-    #[allow(clippy::arithmetic_side_effects)]
-    fn move_text_location(&mut self, direction: &Direction) {
-        let Location { mut x, mut y } = self.location;
+    fn scroll_vertically(&mut self, to: usize) {
         let Size { height, .. } = self.size;
-
-        match direction {
-            Direction::Up => y = y.saturating_sub(1),
-            Direction::Down => y = y.saturating_add(1),
-
-            Direction::Left => {
-                if x > 0 {
-                    x -= 1;
-                } else if y > 0 {
-                    y -= 1;
-                    x = self.buffer.lines.get(y).map_or(0, Line::len);
-                }
-            }
-
-            Direction::Right => {
-                let width = self.buffer.lines.get(y).map_or(0, Line::len);
-                if x < width {
-                    x += 1;
-                } else {
-                    y = y.saturating_add(1);
-                    x = 0;
-                }
-            }
-            Direction::PageUp => y = y.saturating_sub(height).saturating_sub(1),
-            Direction::PageDown => y = y.saturating_add(height).saturating_sub(1),
-            Direction::Home => x = 0,
-            Direction::End => x = self.buffer.lines.get(y).map_or(0, Line::len),
-        }
-        x = self
-            .buffer
-            .lines
-            .get(y)
-            .map_or(0, |line| min(line.len(), x));
-
-        y = min(y, self.buffer.lines.len());
-
-        self.location = Location { x, y };
-        self.scroll_location_into_view();
+        let offset_changed = if to < self.scroll_offset.row {
+            self.scroll_offset.row = to;
+            true
+        } else if to >= self.scroll_offset.row.saturating_add(height) {
+            self.scroll_offset.row = to.saturating_sub(height).saturating_add(1);
+            true
+        } else {
+            false
+        };
+        self.needs_redraw = self.needs_redraw || offset_changed;
     }
 
-    fn resize(&mut self, to: Size) {
-        self.size = to;
-        self.scroll_location_into_view();
-        self.needs_redraw = true;
+    fn scroll_horizontally(&mut self, to: usize) {
+        let Size { width, .. } = self.size;
+        let offset_changed = if to < self.scroll_offset.col {
+            self.scroll_offset.col = to;
+            true
+        } else if to >= self.scroll_offset.col.saturating_add(width) {
+            self.scroll_offset.col = to.saturating_sub(width).saturating_add(1);
+            true
+        } else {
+            false
+        };
+
+        self.needs_redraw = self.needs_redraw || offset_changed
     }
 
-    fn scroll_location_into_view(&mut self) {
-        let Location { x, y } = self.location;
-        let Size { width, height } = self.size;
-        let mut offset_changed = false;
+    fn scroll_text_location_into_view(&mut self) {
+        let Position { row, col } = self.text_location_to_position();
+        self.scroll_vertically(row);
+        self.scroll_horizontally(col);
+    }
 
-        // scroll vertically
-        if y < self.scroll_offset.y {
-            self.scroll_offset.y = y;
-            offset_changed = true;
-        } else if y >= self.scroll_offset.y.saturating_add(height) {
-            self.scroll_offset.y = y.saturating_sub(height).saturating_add(1);
-            offset_changed = true;
-        }
+    pub fn caret_position(&self) -> Position {
+        self.text_location_to_position()
+            .saturating_sub(self.scroll_offset)
+    }
 
-        //scroll horizontally
-        if x < self.scroll_offset.x {
-            self.scroll_offset.x = x;
-            offset_changed = true;
-        } else if x >= self.scroll_offset.x.saturating_add(width) {
-            self.scroll_offset.x = y.saturating_sub(width).saturating_add(1);
-            offset_changed = true;
-        }
-
-        self.needs_redraw = offset_changed;
+    fn text_location_to_position(&self) -> Position {
+        let row = self.text_location.line_index;
+        let col = self.buffer.lines.get(row).map_or(0, |line| {
+            line.width_until(self.text_location.grapheme_index)
+        });
+        Position { col, row }
     }
 
     fn render_line(at: usize, line_text: &str) {
@@ -174,8 +152,8 @@ impl Default for View {
             buffer: Buffer::default(),
             needs_redraw: true,
             size: Terminal::size().unwrap_or_default(),
-            location: Location::default(),
-            scroll_offset: Location::default(),
+            text_location: Location::default(),
+            scroll_offset: Position::default(),
         }
     }
 }
